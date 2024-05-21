330791e6490c643266d189b4526ba807779aee6e
DeeptanshuSanghani1 <deeptanshusanghani@gmail.com>
2024-05-21 18:54:48 -0400

    added experience + Resume update

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -48,8 +48,7 @@ export const Projects = () => {
                                 <div><div class="text-slate-500" aria-hidden="true"></div></div>
                             </h3>
                             <p className="mt-2 text-sm leading-normal">
-                                Delivered high quality robust code that optimized the license plate recognition system. Presented an
-                                multiple algorithms and solutions to the client.
+                                Developed a user friendly Portfolio website displaying my projects, skills, and experiences.
                             </p>
                             <ul class="mt-2 flex flex-wrap">
                                 <li className="mr-1.5 mt-2 ">
@@ -68,6 +67,75 @@ export const Projects = () => {
                                     <div className="flex items-center rounded-full bg-teal-400/10 
                                     px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
                                         Swiper JS
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </li>
+
+
+                <li className="mb-12 group">
+                <div className="group relative grid pb-1 transition-all
+                    phone:grid-cols-8 phone:gap-8 md:gap-4 lg:hover:!opacity-100 
+                    lg:group-hover/list:opacity-50 	shadow-lg hover:shadow-[#6419e6] group-hover:rounded-hover:shadow-2xl">
+                        <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none 
+                        lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 
+                        lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] 
+                        lg:group-hover:drop-shadow-lg">
+                        </div>
+                        <header className="z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 phone:col-span-2">
+                         <div className="ml-2">Dec 2023</div>
+                        </header>
+                        <div className="z-10 phone:col-span-6">
+                            <h3 className='font-medium leading-snug text-slate-200'>
+                                <div>
+                                    <a className="inline-flex items-baseline font-medium
+                                     leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300  
+                                     group/link text-base" href="https://github.com/DeeptanshuSanghani1/Portfolio" target="_blank">
+                                        <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
+                                            <span>
+                                                StackSwap
+                                                <span className="inline-block">
+                                                    
+                                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="inline-block h-4 w-4 shrink-0 
+                                                    transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 
+                                                    group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-px" aria-hidden="true"><path fill-rule="evenodd" 
+                                                    d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 
+                                                    0 000 1.06z" clip-rule="evenodd"></path></svg>
+                                                </span>
+                                            </span>
+                                    </a>
+                                </div>
+                                <div><div class="text-slate-500" aria-hidden="true"></div></div>
+                            </h3>
+                            <p className="mt-2 text-sm leading-normal">
+                                Developed a Card Trading platform in a group of 4. The website allows users to trade cards with other users, purchase cards from a marketplace, provides authentication / authorization
+                                as well as persist user login information and user trade / purchase history.
+                            </p>
+                            <ul class="mt-2 flex flex-wrap">
+                                <li className="mr-1.5 mt-2 ">
+                                    <div className="flex items-center rounded-full bg-teal-400/10 
+                                    px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
+                                        ASP.NET
+                                    </div>
+                                </li>
+                                <li className="mr-1.5 mt-2 ">
+                                    <div className="flex items-center rounded-full bg-teal-400/10 
+                                    px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
+                                        Tailwindcss
+                                    </div>
+                                </li>
+                                <li className="mr-1.5 mt-2 ">
+                                    <div className="flex items-center rounded-full bg-teal-400/10 
+                                    px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
+                                        SQLite
+                                    </div>
+                                </li>
+                                <li className="mr-1.5 mt-2 ">
+                                    <div className="flex items-center rounded-full bg-teal-400/10 
+                                    px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
+                                        Razor Pages
                                     </div>
                                 </li>
                             </ul>
